d313f40cfa0121401144f662fd3aca90da549290
Tomuh <Tommuhika@outlook.com>
2023-06-23 10:27:13 +0300

    landing app links changes

--- mintari/landing/urls.py
+++ mintari/landing/urls.py
@@ -5,6 +5,15 @@ from . import views
 app_name = 'landing'
 
 urlpatterns = [
-    # mintarikenya.com
+    # mintarikenya.co.ke
     re_path(r'^$', views.index, name='index'),
+
+    # mintarikenya.co.ke/contact&us/
+    re_path(r'^contact&us/$', views.contact__us, name='contact_us'),
+
+    # mintarikenya.co.ke/aboutUs/
+    re_path(r'^aboutUs/$', views.about_us, name='aboutUs'),
+
+    # mintarikenya.co.ke/coming_soon/
+    re_path(r'^coming_soon/$', views.coming_soon, name='coming_soon'),
 ]
--- mintari/landing/views.py
+++ mintari/landing/views.py
@@ -5,3 +5,15 @@ from django.contrib.auth.forms import AuthenticationForm
 # mintarikenya.com
 def index(request):
 	return render(request, 'landing/global_index.html')
+
+
+def contact__us(request):
+	return render(request, 'landing/contact_us.html')
+
+
+def coming_soon(request):
+	return render(request, 'landing/coming_soon.html')
+
+
+def about_us(request):
+	return render(request, 'landing/aboutUs.html')
