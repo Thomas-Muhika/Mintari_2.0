8b768996ff5789b0f956978d99332c4401beb1d5
Tomuh <Tommuhika@outlook.com>
2024-10-18 14:00:44 +0300

    featured post

--- mintari/blog/views.py
+++ mintari/blog/views.py
@@ -19,7 +19,7 @@ def index(request):
 		"upholstery_articles": BlogArticle.objects.all().filter(ArticleTag='upholstery')[:4],
 		"latest_articles": BlogArticle.objects.all().order_by('-date_created')[:3],
 		"popular_articles": BlogArticle.objects.all().order_by('date_created')[:4],
-		"featured_article": BlogArticle.objects.all().filter(ArticleTag='featured')[:1],
+		"featured_article": BlogArticle.objects.all().filter(ArticleTag='Featured')[:1],
 	}
 
 	return render(request, 'blog/index.html', context)
