4bb68d61c0e0bf50e4330225c326282670005097
Tomuh <Tommuhika@outlook.com>
2024-10-18 14:23:12 +0300

    featured post in blog-details pg

--- mintari/blog/views.py
+++ mintari/blog/views.py
@@ -29,7 +29,8 @@ class BlogDetails(View):
 	def get(self, request, blog_title):
 
 		context = {
-			"blog_article": BlogArticle.objects.all().filter(ArticleTitle=blog_title)[:1]
+			"blog_article": BlogArticle.objects.all().filter(ArticleTitle=blog_title)[:1],
+			"featured_article": BlogArticle.objects.all().filter(ArticleTag='Featured')[:1],
 		}
 		return render(request, 'blog/blog-details.html', context)
 
